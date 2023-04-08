8a1f291dafe7bc4cbeb55be473c9f5220f2b88f7
Samuel Ferracini <samuelferracini@Samuels-MacBook-Pro.local>
2023-04-08 17:08:41 +0930

    feat: create select input component

---
+++ resources/js/Components/SelectInput.vue
@@ -0,0 +1,46 @@
+<script setup>
+defineProps({
+  modelValue: {
+    type: String,
+    required: false,
+  },
+
+  keyId: {
+    type: String,
+    required: false,
+    default: "id",
+  },
+
+  keyValue: {
+    type: String,
+    required: false,
+    default: "id",
+  },
+
+  keyText: {
+    type: String,
+    required: false,
+    default: "name",
+  },
+
+  items: {
+    type: Array,
+    required: true,
+  },
+});
+
+defineEmits(["update:modelValue"]);
+</script>
+
+<template>
+  <select
+    class="w-full"
+    @change="$emit('update:modelValue', $event.target.value)"
+  >
+    <option value="" selected disabled hidden>Choose here</option>
+
+    <option v-for="item in items" :key="item[keyId]" :value="item[keyValue]">
+      {{ item[keyText] }}
+    </option>
+  </select>
+</template>

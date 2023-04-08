9c372c2e37f05103f40c0948dffed04b4c65c055
Samuel Ferracini <samuelferracini@Samuels-MacBook-Pro.local>
2023-04-08 17:56:51 +0930

    feat: create skills table component

---
+++ resources/js/Pages/Skills/components/SkillsTable/SkillTableItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <tr
+    class="bg-white border-b light:bg-gray-800 light:border-gray-700 hover:bg-gray-50 light:hover:bg-gray-600"
+  >
+    <th
+      scope="row"
+      class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap light:text-white"
+    >
+      {{ skill.id }}
+    </th>
+    <td class="px-6 py-4">{{ skill.name }}</td>
+    <td class="px-6 py-4">
+      {{ skill.image }}
+    </td>
+    <td class="px-6 py-4 text-right">
+      <a
+        href="#"
+        class="font-medium text-blue-600 light:text-blue-500 hover:underline"
+        >Edit</a
+      >
+    </td>
+  </tr>
+</template>
+
+<script setup>
+defineProps({
+  skill: Object,
+});
+</script>
---
+++ resources/js/Pages/Skills/components/SkillsTable/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500 light:text-gray-400">
+      <thead
+        class="text-xs text-gray-700 uppercase bg-gray-50 light:bg-gray-700 light:text-gray-400"
+      >
+        <tr>
+          <th scope="col" class="px-6 py-3">Id</th>
+          <th scope="col" class="px-6 py-3">Name</th>
+          <th scope="col" class="px-6 py-3">Image</th>
+          <th scope="col" class="px-6 py-3">
+            <span class="sr-only">Edit</span>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <SkillTableItem v-for="skill in skills" :skill="skill" />
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup>
+import SkillTableItem from "./SkillTableItem.vue";
+
+defineProps({
+  skills: Array,
+});
+</script>

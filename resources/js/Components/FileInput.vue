74fe85607bd607660c83e6c97e6f626003bbaa29
Samuel Ferracini <samuelferracini@Samuels-MacBook-Pro.local>
2023-04-08 15:30:25 +0930

    feat: create file input

---
+++ resources/js/Components/FileInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <input
+    accept="image/png, image/gif, image/jpeg"
+    type="file"
+    required
+    @input="$emit('update:modelValue', $event.target.files[0])"
+  />
+</template>
+<script setup>
+defineProps({
+  modelValue: {
+    type: File,
+    required: false,
+  },
+});
+
+defineEmits(["update:modelValue"]);
+</script>

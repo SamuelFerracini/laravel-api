df35135d4e89778d1771184dcb327b1d98b5a4fc
Samuel Ferracini <samuelferracini@Samuels-MacBook-Pro.local>
2023-04-09 08:54:10 +0930

    feat: create projects table component

---
+++ resources/js/Pages/Projects/components/ProjectsTable/ProjectTableItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <tr
+    class="bg-white border-b light:bg-gray-800 light:border-gray-700 hover:bg-gray-50 light:hover:bg-gray-600"
+  >
+    <th
+      scope="row"
+      class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap light:text-white"
+    >
+      {{ project.id }}
+    </th>
+    <td class="px-6 py-4">{{ project.name }}</td>
+    <td class="px-6 py-4">{{ project.skill.name }}</td>
+    <td class="px-6 py-4">
+      <img :src="project.image" class="w-12 h-12 rounded-full" />
+    </td>
+    <td class="px-6 py-4 text-right flex flex-col">
+      <a
+        href="#"
+        class="font-medium text-blue-600 light:text-blue-500 hover:underline"
+      >
+        Edit
+      </a>
+      <a
+        href="#"
+        class="font-medium text-blue-600 light:text-blue-500 hover:underline"
+      >
+        Delete
+      </a>
+    </td>
+  </tr>
+</template>
+
+<script setup>
+defineProps({
+  project: Object,
+});
+</script>
---
+++ resources/js/Pages/Projects/components/ProjectsTable/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500 light:text-gray-400">
+      <thead
+        class="text-xs text-gray-700 uppercase bg-gray-50 light:bg-gray-700 light:text-gray-400"
+      >
+        <tr>
+          <th scope="col" class="px-6 py-3">Id</th>
+          <th scope="col" class="px-6 py-3">Name</th>
+          <th scope="col" class="px-6 py-3">Skill</th>
+          <th scope="col" class="px-6 py-3">Image</th>
+          <th scope="col" class="px-6 py-3">
+            <span class="sr-only">Edit</span>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <ProjectTableItem v-for="project in projects" :project="project" />
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup>
+import ProjectTableItem from "./ProjectTableItem.vue";
+
+defineProps({
+  projects: Array,
+});
+</script>

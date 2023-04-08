a87862def9b15d3f5d72c90b9b4d7f3c8e75b73a
Samuel Ferracini <samuelferracini@Samuels-MacBook-Pro.local>
2023-04-08 17:07:02 +0930

    feat: create store project request

---
+++ app/Http/Requests/StoreProjectRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProjectRequest extends FormRequest
+{
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+   */
+  public function rules(): array
+  {
+    return [
+      'name' => ['required', 'string', 'max:255'],
+      'image' => ['required', 'image', 'mimes:jpg,png,jpeg,gif,svg', 'max:2048'],
+      'project_url' => ['required', 'url'],
+      'skill_id' => ['required', 'exists:skills,id'],
+    ];
+  }
+}

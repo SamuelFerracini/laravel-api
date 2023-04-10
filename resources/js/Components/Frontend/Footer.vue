5e7120dbc2fabce4324cd017fc2a3e0d33b63e99
Samuel Ferracini <samuelferracini@Samuels-MacBook-Pro.local>
2023-04-10 17:52:34 +0930

    refactor: make footer sticky

--- resources/js/Components/Frontend/Footer.vue
+++ resources/js/Components/Frontend/Footer.vue
@@ -1,6 +1,8 @@
 <script setup></script>
 <template>
-  <footer class="bg-light-secondary dark:bg-dark-secondary py-12">
+  <footer
+    class="bg-light-secondary dark:bg-dark-secondary py-4 fixed bottom-0 w-full"
+  >
     <div class="container mx-auto">
       <div
         class="flex flex-col md:flex-row space-y-6 lg:space-y-0 items-center justify-between"
